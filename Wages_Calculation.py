fe87dc08879187a361d9333401ae6898bf15355a
NourOueghlani2 <nour.oueghlani@forvismazars.com>
2025-10-28 18:36:03 +0100

    yes

--- Wages_Calculation.py
+++ Wages_Calculation.py
@@ -1,9 +1,20 @@
 import pandas as pd
 
+
+
 ############ Name of the file to be read ############
-# C'est votre chemin, il est correct
-Wages_File = r'C:\Users\nour.oueghlani\Documents\Hackathon\STATEC-Hackathon\Data\Benefits\Annual wages 1990-2024.xlsx'
+
+
+
+Wages_File = r'C:\Users\nour.oueghlani\Documents\Hackathon\STATEC-Hackathon\Data\Benefits\Annual wages.xlsx'
+Income_File = r'C:\Users\nour.oueghlani\Documents\Hackathon\STATEC-Hackathon\Data\Benefits\Income per year - cleaned_version.xls'
+Tableau_Output= pd.
 
 ################ Reading the Excel file ############
+Income_File_dated_2012 = pd.read_excel(Income_File, header=None)
+Income_File
+Wages_data_annually = pd.read_excel(Wages_File, header=0)
 
-df_data = pd.read_excel(Wages_File)
+################ Treatment of the data ############
+Wages_data_annually = Wages_data_annually.set_index('Time period')
+print(Income_File_dated.head())
