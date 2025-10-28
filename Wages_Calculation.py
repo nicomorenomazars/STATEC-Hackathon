29e334a8a2563d60fc83f779b5cb3d616eb34d89
Nikita Gaponiuk <nikita.gaponiuk@mazars.lu>
2025-10-28 18:21:01 +0100

    sync

--- Wages_Calculation.py
+++ Wages_Calculation.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 ############ Name of the file to be read ############
 # C'est votre chemin, il est correct
-Wages_File = r'C:\Users\nour.oueghlani\Documents\Hackathon\STATEC-Hackathon\Data\Benefits\Annual wages 1990-2024.xlsx'
+Wages_File = '.\Data\Benefits\Annual wages 1990-2024.xlsx'
 
 ################ Reading the Excel file ############
 
