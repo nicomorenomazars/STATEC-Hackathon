86a9a2021f137728c47e66728bef3c96c040021c
Nikita Gaponiuk <nikita.gaponiuk@mazars.lu>
2025-10-29 13:53:08 +0100

    sync

--- merge.py
+++ merge.py
@@ -620,7 +620,7 @@ final_combined_df.loc[final_combined_df['Year'] > 2024, 'Adjustment_factor_1984'
 
 final_combined_df.loc[final_combined_df['Year'] < 1970, 'Adjustment_factor_1984'] = 150
 
-final_combined_df['Adjustment_factor_1984'] = final_combined_df['Adjustment_factor_1984'] / 432.37
+# final_combined_df['Adjustment_factor_1984'] = final_combined_df['Adjustment_factor_1984'] / 432.37
 
 # --- 8.4 Final Save ---
 # Sort and save
